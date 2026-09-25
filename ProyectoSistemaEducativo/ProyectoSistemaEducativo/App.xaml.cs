9fdda08a83cad68deef961fd9394e715f8db52c2
María López <lopezbrbelen@gmail.com>
2026-09-25 18:33:42 -0300

    Proyecto completo sin temporales

---
+++ ProyectoSistemaEducativo/ProyectoSistemaEducativo/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ProyectoSistemaEducativo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ ProyectoSistemaEducativo/ProyectoSistemaEducativo/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProyectoSistemaEducativo
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
